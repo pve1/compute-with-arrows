310f4ffea0611db1fccbb739166890036e29824a
pve1 <pve1@foo.invalid>
2022-08-03 12:40:36 +0300

    Substitution.

--- substitution.lisp
+++ substitution.lisp
@@ -1,19 +1,24 @@
 (in-package :compute-with-arrows)
 
-(defun make-substitution (&key new rest consumed)
-  (list :new new
+(defun make-substitution (&key new rest consumed type)
+  (list 'substitution
+        :new new
+        :type type
         :consumed consumed
         :rest rest))
 
 (defun substitutionp (substitution)
   (and (listp substitution)
-       (eq (car substitution) :new)))
+       (eq (car substitution) 'substitution)))
 
 (defun substitution-new (substitution)
-  (getf substitution :new))
+  (getf (cdr substitution) :new))
 
 (defun substitution-rest (substitution)
-  (getf substitution :rest))
+  (getf (cdr substitution) :rest))
 
 (defun substitution-consumed (substitution)
-  (getf substitution :consumed))
+  (getf (cdr substitution) :consumed))
+
+(defun substitution-type (substitution)
+  (getf (cdr substitution) :type))
